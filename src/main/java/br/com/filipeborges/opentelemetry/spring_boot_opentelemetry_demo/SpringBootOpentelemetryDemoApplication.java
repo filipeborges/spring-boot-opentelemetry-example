933be68398ef3e021bd1a31a393f95170476ed1e
Filipe Borges <filipebkc2209@gmail.com>
2025-02-27 12:42:06 -0300

    Initial commit

---
+++ src/main/java/br/com/filipeborges/opentelemetry/spring_boot_opentelemetry_demo/SpringBootOpentelemetryDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.filipeborges.opentelemetry.spring_boot_opentelemetry_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpentelemetryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOpentelemetryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/filipeborges/opentelemetry/spring_boot_opentelemetry_demo/SpringBootOpentelemetryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filipeborges.opentelemetry.spring_boot_opentelemetry_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpentelemetryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
