4a7a8eae81e274d2d7987242e46085d59997b86b
Filipe Borges <filipebkc2209@gmail.com>
2025-03-24 21:44:26 -0300

    Added script to generate random http requests and adjusted collector grep

--- start-compose-with-collector-grep.sh
+++ start-compose-with-collector-grep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker-compose up | grep -E 'Span #|Attributes|\-> messaging|(ID|Name|Kind|time|Status \w+)\s+:'
+sudo docker-compose up | grep -E 'Span #|Attributes|\-> |(ID|Name|Kind|time|Status \w+)\s+:'
