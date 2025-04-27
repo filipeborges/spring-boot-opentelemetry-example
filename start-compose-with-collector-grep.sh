188413fa07abf40fae1e7b8fa63fa9da37d36234
Filipe Borges <filipebkc2209@gmail.com>
2025-04-27 16:52:17 -0300

    Added trace context info on logs and sending logs to collector

--- start-compose-with-collector-grep.sh
+++ start-compose-with-collector-grep.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker-compose up | grep -E 'Span #|Attributes|\-> |(ID|Name|Kind|time|Status \w+)\s+:'
+sudo docker-compose up | grep -E 'Span #|Attributes|\-> |(ID|Name|Kind|time|Status \w+)\s+:|(ScopeLogs|InstrumentationScope|LogRecord|Timestamp|Severity|Body|Trace|Span)'
